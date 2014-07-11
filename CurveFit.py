445b9e3e9f416f824e0f3ac4378a44e3d8d7ab73
Your Name <sgpthomas@gmail.com>
2014-07-11 08:48:07 -0700

    Curve Fitting

--- CurveFit.py
+++ CurveFit.py
@@ -7,6 +7,7 @@ Created on Wed Jul  9 22:07:57 2014
 #imports
 import numpy as np
 import Langmuir as lm
+import matplotlib.pyplot as plt
 
 def makeArrayWithFit(fit, size):
     arr = np.arange(0, size, 1.0)
@@ -17,9 +18,41 @@ def makeArrayWithFit(fit, size):
         count += 1
         
     return arr
-    
+
+#data    
 data = lm.run()
 v = data[0] * -1
-v = v[1800:2800]
-cof = np.polyfit(np.arange(1000), v, 3)
-fit = makeArrayWithFit(cof, 1000)
+
+#first curve fit
+vRef = v[1800:2800]
+curveCof = np.polyfit(np.arange(1000), vRef, 3)
+curveFit = makeArrayWithFit(curveCof, 1000)
+
+#extended curve in line
+points = curveFit[998:1000] #take last two points of fit
+lineCof = np.polyfit(np.arange(2), points, 1)
+extlineFit = [lineCof[0] * c + lineCof[1] for c in np.arange(1000)]
+
+#line fit the top portion
+size = (3800, 4800)
+top = v[size[0]:size[1]]
+topCof = np.polyfit(np.arange(size[0],size[1]), top, 1)
+topLineFit = [topCof[0] * c + topCof[1] for c in np.arange(size[0] - 1000,size[1])]
+
+#plotting
+plt.plot(v, "y", label = "Test", linewidth = 1) #main curve
+plt.plot(np.arange(1800, 2800), curveFit, "b", linewidth = 2) #fitted curve 3rd degree
+plt.plot(np.arange(2800, 3800), extlineFit, "b--", linewidth = 2) #fitted line
+plt.plot(np.arange(size[0] - 1000, size[1]), topLineFit, "r", linewidth = 2)
+
+#showing plot
+plt.xlabel("Index")
+plt.ylabel("Current")
+plt.show()
+
+
+
+
+
+
+
